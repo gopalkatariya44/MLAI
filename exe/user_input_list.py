4f357a5a91e95d09635eb727ebed8bf86641d74c
Gopal Katariya <katariyagopal60@gmail.com>
2022-02-25 09:11:43 +0530

    all set

---
+++ exe/user_input_list.py
@@ -0,0 +1,16 @@
+# list = []
+
+# for i in range(len(list)):
+#     n_list = int(input('Enter the No. of List : '))
+# import string
+# Line1 = "And Then There Were None"
+# Line2 = "Famous In Love"
+# Line3 = "Famous Were The Kol And Klaus"
+# Line4 = Line1 + Line2 + Line3
+# print("And" in Line4)
+
+a = [1,2,3,4]
+b = [1,2,3,4]
+
+a.extend(b)
+print(a)
